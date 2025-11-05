dac3e29a664be0273d9bd4ce85cc9c20f3068b45
sxsorensen <sxsorensen2@students.everettcc.edu>
2025-11-05 04:40:04 +0000

    Final changes For Loop

--- main.py
+++ main.py
@@ -5,32 +5,30 @@
 #Generate the list of missions launched before the year 2000
 #Print results to the screen
 
-#MISSION LISTS
+#MISSION LISTS - CREATE REFERENCE LIST VARIABLES
 mission_names = ["Apollo 11", "Challenger", "Curiosity Rover", "Viking 1", "Mars Pathfinder", "Hubble Telescope", "Apollo 13"]
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
 
 #MISSION COUNTS AND SUCCESS RATE CALCULATIONS
-mission_count = len(mission_names)
-mission_success_count = int(0)
-for x in mission_success:
+mission_count = len(mission_names) #count of missions
+mission_success_count = int(0) #define variable for successes as integer
+for x in mission_success: #for loop to count the number of successes found
     if x == True:
         mission_success_count += 1
-mission_success_rate = float(mission_success_count / mission_count)
-rounded_success_rate = round(mission_success_rate, 4) * 100
+mission_success_rate = float(mission_success_count / mission_count) #calculates the success rates
+rounded_success_rate = round(mission_success_rate, 4) * 100 #converts success rate to a rounded percent
 
 #MISSIONS PRIOR TO THE YEAR 2000
-pre2k_missions = []
-for x in mission_years:
-    if x > 2000: 
-        pre2k_missions.extend(mission_names)
+pre2k_missions = [] #define the list to populate
+for n, y in zip(mission_names, mission_years): #for loop to generate a list of mission prior to 2000
+    if y < 2000: 
+        pre2k_missions.append(n) #save the results to the list
 
 #OUTPUT RESULTS TO SCREEN
 print("Total number of missions:", mission_count)
 print("Number of successful missions:", mission_success_count)
 print("Success rate:", str(rounded_success_rate) + "%")
 print("Missions launched before the year 2000:")
-for i in pre2k_missions: 
+for i in pre2k_missions: #for loop to print the list items one at a time with formatting
     print("-", i)
-
-#print(pre2k_missions)
