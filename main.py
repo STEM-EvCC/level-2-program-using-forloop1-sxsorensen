6432c9a52bcaad4ed8f2df6a66b6dd816ec8813e
sxsorensen <sxsorensen2@students.everettcc.edu>
2025-11-05 05:59:35 +0000

    Final

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ mission_success_count = int(0) #define variable for successes as integer
 for x in mission_success: #for loop to count the number of successes found
     if x == True:
         mission_success_count += 1
-mission_success_rate = float(mission_success_count / mission_count) #calculates the success rates
-rounded_success_rate = round(mission_success_rate, 4) * 100 #converts success rate to a rounded percent
+mission_success_rate = float(mission_success_count / mission_count * 100) #calculates the success rates
 
 #MISSIONS PRIOR TO THE YEAR 2000
 pre2k_missions = [] #define the list to populate
@@ -28,7 +27,7 @@ for n, y in zip(mission_names, mission_years): #for loop to generate a list of m
 #OUTPUT RESULTS TO SCREEN
 print("Total number of missions:", mission_count)
 print("Number of successful missions:", mission_success_count)
-print("Success rate:", str(rounded_success_rate) + "%")
+print(f"Success rate: {mission_success_rate:.2f}%")
 print("Missions launched before the year 2000:")
 for i in pre2k_missions: #for loop to print the list items one at a time with formatting
     print("-", i)
